c58e93b2b5f8d7c1a4d3877d7848656a46ba98b1
Adenilton Pereira <adenilton_23@yahoo.com.br>
2020-05-08 23:38:02 -0300

    Estrutura das Classes Finalizada

---
+++ Pedido/Entities/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pedido.Entities.Enums;
+
+namespace Pedido.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }       
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+       
+        public List<OrderItem> items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;          
+        }
+        public void AddItem(OrderItem item)
+        {
+            items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            items.Remove(item);
+        }
+        public double Total()
+        {
+            double sum = 0.0;
+             foreach(OrderItem item in items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+    }
+}
---
+++ Pedido/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pedido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
